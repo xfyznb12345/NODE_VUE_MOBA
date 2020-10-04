e016d527739eddd0a35b85f2cb1bc44d84e63680
徐飞杨 <505673373@qq.com>
2020-10-04 10:04:56 +0800

    后台

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,28 +1,121 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue'),
+    meta:{ isPublic: true }
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/',
+    name: 'Main',
+    component: () => import('../views/Main.vue'),
+    children: [
+      {
+        path: '/items/create',
+        name: 'ItemEdit',
+        component: () => import('../views/ItemEdit.vue'),
+      },
+      {
+        path: '/items/edit',
+        name: 'ItemEdit',
+        component: () => import('../views/ItemEdit.vue'),
+      },
+      {
+        path: '/items/list',
+        name: 'ItemList',
+        component: () => import('../views/ItemList.vue'),
+      },
+      {
+        path: '/ads/create',
+        name: 'AdEdit',
+        component: () => import('../views/AdEdit.vue'),
+      },
+      {
+        path: '/ads/edit',
+        name: 'AdEdit',
+        component: () => import('../views/AdEdit.vue'),
+      },
+      {
+        path: '/ads/list',
+        name: 'AdList',
+        component: () => import('../views/AdList.vue'),
+      },
+      {
+        path: '/admin_users/create',
+        name: 'AdminUserEdit',
+        component: () => import('../views/AdminUserEdit.vue'),
+      },
+      {
+        path: '/admin_users/edit',
+        name: 'AdminUserEdit',
+        component: () => import('../views/AdminUserEdit.vue'),
+      },
+      {
+        path: '/admin_users/list',
+        name: 'AdminUserList',
+        component: () => import('../views/AdminUserList.vue'),
+      },
+      {
+        path: '/articles/create',
+        name: 'ArticleEdit',
+        component: () => import('../views/ArticleEdit.vue'),
+      },
+      {
+        path: '/articles/edit',
+        name: 'ArticleEdit',
+        component: () => import('../views/ArticleEdit.vue'),
+      },
+      {
+        path: '/articles/list',
+        name: 'ArticleList',
+        component: () => import('../views/ArticleList.vue'),
+      },
+      {
+        path: '/categories/create',
+        name: 'CategoryEdit',
+        component: () => import('../views/CategoryEdit.vue'),
+      },
+      {
+        path: '/categories/edit',
+        name: 'CategoryEdit',
+        component: () => import('../views/CategoryEdit.vue'),
+      },
+      {
+        path: '/categories/list',
+        name: 'CategoryList',
+        component: () => import('../views/CategoryList.vue'),
+      },
+      {
+        path: '/heroes/create',
+        name: 'HeroEdit',
+        component: () => import('../views/HeroEdit.vue'),
+      },
+      {
+        path: '/heroes/edit',
+        name: 'HeroEdit',
+        component: () => import('../views/HeroEdit.vue'),
+      },
+      {
+        path: '/heroes/list',
+        name: 'HeroList',
+        component: () => import('../views/HeroList.vue'),
+      },
+    ]
   }
-];
+]
 
 const router = new VueRouter({
-  routes
-});
-
-export default router;
+  routes,
+})
+router.beforeEach((to, from ,next) => {
+  if (!to.meta.isPublic && !localStorage.token) {
+    return next('/login')
+  }
+  next()
+})
+export default router
--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ const express = require('express')
 
 const app = express()
 
+app.set('secret', 'i2u34y12oi3u4y8')
+
+app.use(require('cors')())
+app.use(express.json())
+
+require('./plugins/db')(app)
+require('./routes/admin')(app)
+app.use('/uploads', express.static(__dirname + '/uploads'))
+
 
 app.listen(3000,()=>{
   console.log('http://localhost:3000');
